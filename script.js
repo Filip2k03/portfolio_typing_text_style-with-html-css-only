a179424f93abf26d892738ba9ff5630ba957f304
Filip2k03 <stephanfilip7@gmail.com>
2025-02-12 12:54:49 +0630

    ad update

---
+++ script.js
@@ -0,0 +1,29 @@
+  // Google Analytics
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+
+  // Default consent
+  gtag('consent', 'default', {
+    ad_storage: 'denied',
+    analytics_storage: 'denied',
+    wait_for_update: 500
+  });
+
+  // Update on consent
+  function allConsentGranted() {
+    gtag('consent', 'update', {
+      ad_storage: 'granted',
+      analytics_storage: 'granted'
+    });
+    loadAds();
+  }
+
+  // Delayed ad loading
+  function loadAds() {
+    setTimeout(() => {
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    }, 1000);
+  }
+
+
+ 
